3ccc33e905f593a38f22647d0980690592b5c041
LakshayySharma <44471078+LakshayySharma@users.noreply.github.com>
2023-01-26 20:59:32 +0530

    basic annotation

--- fetchJson.ts
+++ fetchJson.ts
@@ -1,4 +1,24 @@
 import axios from "axios";
 
-let res = await axios.get("https://jsonplaceholder.typicode.com/posts/1");
-console.log(res.data);
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+async function getUrl() {
+  let res = await axios.get("https://jsonplaceholder.typicode.com/posts/1");
+  let data: Post = res.data;
+  let { userId, id, title, body } = data;
+  printData(userId, id, title, body);
+}
+
+function printData(userId: number, id: number, title: string, body: string) {
+  console.log(userId);
+  console.log(id);
+  console.log(title);
+  console.log(body);
+}
+
+getUrl();
