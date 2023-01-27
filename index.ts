d22a666cddb2d1663af86c3a6cefa70ab96266a0
LakshayySharma <44471078+LakshayySharma@users.noreply.github.com>
2023-01-27 16:54:30 +0530

    typed arrays

---
+++ index.ts
@@ -0,0 +1,11 @@
+//Typed array
+const colors: string[] = ["red", "green", "yellow", "blue"];
+const numbers: number[] = [1, 2, 3];
+const bool: boolean[] = [true, false];
+
+const grid: number[][] = [
+  [1, 2, 3],
+  [4, 5, 6],
+];
+
+const mixed: (string | number)[] = [1, 2, "a"];
